335601eb073eded35aae508b8ec13ea672e84ded
Ed Ilyin <ed.il@live.com>
2017-07-11 13:56:24 +0300

    now some fable-core files got compiled

--- src/App/splitter.config.js
+++ src/App/splitter.config.js
@@ -1,16 +1,26 @@
-const fableSplitter = require("fable-splitter").default;
+const path = require("path");
+const fableSplitter = require("fable-splitter").default
+
+function resolve(filePath) {
+  return path.resolve(__dirname, filePath)
+}
 
 const babelOptions = {
-    plugins: [
-        ["transform-es2015-modules-commonjs"],
-    ],
-    sourceMaps: true
-};
+  plugins: [
+    ["transform-es2015-modules-commonjs"],
+  ],
+  sourceMaps: true
+}
+
+const fableOptions = {
+  fableCore: resolve("./packages/Fable.Core/fable-core")
+}
 
 const options = {
-    entry: "./App.fsproj",
-    outDir: "./out",
-    babel: babelOptions
-};
+  entry: "./App.fsproj",
+  outDir: "./out",
+  babel: babelOptions,
+  fable: fableOptions
+}
 
-fableSplitter(options);
+fableSplitter(options)
